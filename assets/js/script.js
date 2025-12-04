87828c846686119e1897e15f56b4fdf46ca6bc8b
Erick Silva <donizttierick@gmail.com>
2025-12-04 15:16:11 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@
 //MENU MOBILE
 
 function ClickMenu() {
-    if (mobile.style.display == 'block') {
-        mobile.style.display = 'none'
-    }else {
-    mobile.style.display = 'block'
-    }
+   if ( mobile.style.display == 'block') {
+     mobile.style.display = 'none'
+   }else {
+     mobile.style.display = 'block'
+   }
 }
